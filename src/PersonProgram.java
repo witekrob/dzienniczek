4dcaf19d23d28703cb0f94fd83332ee5f1759a50
witekrob <39655905+witekrob@users.noreply.github.com>
2018-09-24 19:34:03 +0100

    Add files via upload

---
+++ src/PersonProgram.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class PersonProgram {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		choiceMenu cM = new choiceMenu();
+
+	}
+}
